6e646ecb765433483b02462d3688709ff0a61e0e
marcustrummer <marcusstrummer1@gmail.com>
2021-07-10 17:29:09 -0300

    Adicionando system.properties para o heroku reconhecer a versao do java 11

---
+++ src/main/java/one/digitalinnovation/personapi/services/PersonService.java
@@ -0,0 +1,26 @@
+package one.digitalinnovation.personapi.services;
+
+import one.digitalinnovation.personapi.controller.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class PersonService {
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID"+savedPerson.getId())
+                .build();
+    }
+}
